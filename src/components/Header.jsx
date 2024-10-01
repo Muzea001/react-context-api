a68be65d2bc4860c61cbc70b8355d8af53fae63c
Muzea001 <muat0072@gmail.com>
2024-10-01 13:40:40 +0200

    Part 1 & 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
-export default function Header({ user, theme, setTheme }) {
+import { useContext } from 'react'
+import { UserContext } from "../App";
+import { ThemeContext } from '../App';
+
+export default function Header() {
+    const { user } = useContext(UserContext);
+    const { theme, setTheme } = useContext(ThemeContext)
+    
     const handleCheckChange = () => {
-      if(theme === 'dark') {
-        setTheme('light');
-      } else {
-        setTheme('dark');
-      }
+        setTheme((prevTheme) => {
+            const newTheme = prevTheme === "light" ? "dark" : "light";
+            localStorage.setItem("theme", newTheme);
+            return newTheme;
+        });
     }
 
     const handleButtonClick = () => {
-      console.log("CLICK!");
+      localStorage.removeItem("theme")
+      setTheme("light")
     }
 
     return (
